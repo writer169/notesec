0c1a01e9b07e6a3f1679265b6e4efa4011f52908
writer169 <148839750+writer169@users.noreply.github.com>
2025-03-14 19:42:02 +0600

    Create Modal.js

---
+++ components/Modal.js
@@ -0,0 +1,9 @@
+// --- components/Modal.js ---
+import React from 'react';
+
+const Modal = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+      <div className="
