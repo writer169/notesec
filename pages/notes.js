3eaf68a5ffb2b1e2c9fcf1b37fca8c700e35b55a
writer169 <148839750+writer169@users.noreply.github.com>
2025-03-14 18:38:56 +0600

    Update notes.js

--- pages/notes.js
+++ pages/notes.js
@@ -1,27 +1,9 @@
 // pages/notes.js
-import { useSession } from 'next-auth/react';
-
-function NotesPage() {
-  const { data: session, status } = useSession();
-
-  if (status === "loading") {
-    return <p>Loading...</p>;
-  }
-
-  if (status === "unauthenticated") {
-    return <p>Access Denied</p>;
-  }
-
-    if (status === "authenticated") {
-    return (
-      <div>
-        <h1>My Notes</h1>
-        <p>This is the notes page. You are logged in as {session.user.email}.</p>
-        {/* Здесь будет твой NoteList и NoteEditor */}
-      </div>
-    );
-  }
-  return <p>Something went wrong</p>
+export default function NotesPage() {
+  return (
+    <div>
+      <h1>Notes Page</h1>
+      <p>This is a test.</p>
+    </div>
+  );
 }
-
-export default NotesPage;
