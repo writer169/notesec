1aef4db108488ef08f7f8599354283a5462507bc
writer169 <148839750+writer169@users.noreply.github.com>
2025-03-14 17:37:30 +0600

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+// pages/_app.js
+import { SessionProvider } from "next-auth/react"
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
+}
+
+export default MyApp
